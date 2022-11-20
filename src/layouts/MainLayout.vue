d143f468f65855c468bcbfae9052a5614c598565
matheuslealaquino <matheusleal.a@gmail.com>
2022-11-20 18:14:25 -0300

    feat: increase layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,15 +11,17 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>Rede Social</q-toolbar-title>
+        <q-toolbar-title>
+          <q-btn to="/" dense flat>Rede Social</q-btn>
+        </q-toolbar-title>
 
         <q-btn flat round dense to="/signup">Cadastre-se</q-btn>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Links importantes: </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -41,46 +43,16 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
+    title: 'Sobre',
+    caption: 'Sobre a rede social',
     icon: 'school',
-    link: 'https://quasar.dev',
+    link: 'https://github.com/MatheusLealAquino/social-network-front',
   },
   {
     title: 'Github',
-    caption: 'github.com/quasarframework',
+    caption: 'github.com/MatheusLealAquino',
     icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    link: 'https://github.com/MatheusLealAquino/social-network-front',
   },
 ];
 
