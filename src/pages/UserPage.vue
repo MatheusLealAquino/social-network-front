288b655d0d7535b594f647d1155afcce4a768257
matheuslealaquino <matheusleal.a@gmail.com>
2023-01-08 22:58:56 -0300

    feat: initialize user page

---
+++ src/pages/UserPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row justify-center">Ola Mundo</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import { useUserStore } from 'stores/user';
+
+export default defineComponent({
+  name: 'UserPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
